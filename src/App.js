c8bd0d57bb5c93007e396f72b4ec119870c1f303
mdray <mdray@live.com>
2022-06-12 17:45:55 -0500

    change text

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nervous Robot Support Group coming soon. 
         </p>
-        <a
+/*       <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+*/      </header>
     </div>
   );
 }
